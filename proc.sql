b2a5719b3d95cb8bf2caf480643ddce80d55e220
Dexter Tan <dev@dextertanyj.com>
2021-10-09 11:03:45 +0800

    Add guards for removed entities

--- proc.sql
+++ proc.sql
@@ -1,3 +1,15 @@
+CREATE OR REPLACE FUNCTION removed_department_guard
+(IN department_id INT, OUT resigned BOOLEAN)
+RETURNS BOOLEAN AS $$
+    SELECT CASE WHEN D.removal_date IS NULL THEN false ELSE true END AS resigned FROM Departments AS D WHERE D.id = department_id;
+$$ LANGUAGE sql;
+
+CREATE OR REPLACE FUNCTION resigned_employee_guard
+(IN employee_id INT, OUT resigned BOOLEAN)
+RETURNS BOOLEAN AS $$
+    SELECT CASE WHEN E.resignation_date IS NULL THEN false ELSE true END AS resigned FROM Employees AS E WHERE E.id = employee_id;
+$$ LANGUAGE sql;
+
 CREATE OR REPLACE PROCEDURE add_department 
 (id INT, name VARCHAR(255)) 
 AS $$
@@ -14,8 +26,14 @@ CREATE OR REPLACE PROCEDURE add_room
 (floor INT, room INT, name VARCHAR(255), capacity INT, department_id INT, manager_id INT, effective_date DATE)
 AS $$
 BEGIN
-    IF (department_id != (SELECT E.department_id FROM Managers AS M JOIN Employees AS E ON M.id = E.id WHERE M.id = manager_id)) THEN
+    IF (department_id <> (SELECT E.department_id FROM Employees AS E WHERE E.id = manager_id)) THEN
         RAISE EXCEPTION 'Manager department and room department mismatch' USING HINT = 'Manager and new room should belong to the same department';
+    END IF;
+    IF (SELECT * FROM resigned_employee_guard(manager_id)) THEN
+        RAISE EXCEPTION 'Manager has resigned' USING HINT = 'Manager has resigned and can no longer add rooms';
+    END IF;
+    If (SELECT * FROM removed_department_guard(department_id)) THEN
+        RAISE EXCEPTION 'Department has been removed' USING HINT = 'Department has been removed and no new rooms can be assigned to it';
     END IF;
     INSERT INTO MeetingRooms VALUES (floor, room, name, department_id);
     INSERT INTO Updates VALUES (manager_id, floor, room, effective_date, capacity);
@@ -26,8 +44,11 @@ CREATE OR REPLACE FUNCTION add_employee
 (IN name VARCHAR(255), IN contact_number VARCHAR(20), IN type VARCHAR(7), IN department_id INT, OUT employee_id INT, OUT employee_email VARCHAR(255))
 RETURNS RECORD AS $$
 BEGIN
+    If (SELECT * FROM removed_department_guard(department_id)) THEN
+        RAISE EXCEPTION 'Department has been removed' USING HINT = 'Department has been removed and no new employees can be assigned to it';
+    END IF;
     INSERT INTO Employees (name, contact_number, email, department_id) VALUES (name, contact_number, ' ', department_id) RETURNING Employees.id INTO employee_id;
-    employee_email = employee_id||'@company.com';
+    employee_email := employee_id||'@company.com';
     UPDATE Employees SET email = employee_email WHERE id = employee_id;
     IF (LOWER(type) = 'junior') THEN 
         INSERT INTO Juniors VALUES (employee_id);
