d37ee2fb20e4fdd6f2484e725ad87b4eacb3ed16
junlong4321 <desmondwongjunlong@yahoo.com.sg>
2021-10-11 23:54:05 +0800

    Add healthdeclarations table

--- Data/data_generator.py
+++ Data/data_generator.py
@@ -3,6 +3,7 @@ import random
 from faker import Faker
 from collections import defaultdict
 from sqlalchemy import create_engine
+import datetime
 
 # Set up Python to PSQL connection
 username = 'postgres'
@@ -18,7 +19,7 @@ for _ in range(20):
     department_data['name'].append(fake.company())
     # Generate random removal_date with an arbitrarily chosen 10% chance
     if (random.random() < 0.1):
-        department_data['removal_date'].append(fake.date_between(start_date='-5y', end_date='today'))
+        department_data['removal_date'].append(fake.date_between(start_date=datetime.date(2016,1,1), end_date='today'))
     else:
         department_data['removal_date'].append(None)
 df_department_data = pd.DataFrame(department_data)
@@ -36,7 +37,7 @@ for _ in range(20):
     employee_data['email'].append(first_name.lower() + '.' + last_name.lower() + '@' + fake.free_email_domain())
     # Generate random resignation_date with an arbitrarily chosen 10% chance
     if (random.random() < 0.1):
-        employee_data['resignation_date'].append(fake.date_between(start_date='-5y', end_date='today'))
+        employee_data['resignation_date'].append(fake.date_between(start_date=datetime.date(2016,1,1), end_date='today'))
     else:
         employee_data['resignation_date'].append(None)
     employee_data['department_id'].append(random.randint(0,19))
@@ -47,15 +48,18 @@ df_employee_data.to_sql(name = 'employees', con = engine, if_exists = 'append')
 
 # Generate meetingrooms table
 meeting_room_data = defaultdict(list)
-floor = 1
-room = 1
-for _ in range(20):
-    if (room == 10):
-        room = 1
-        floor = floor + 1
+meeting_room_pk = []
+for _ in range(30):
+    floor = random.randint(1,5)
+    room = random.randint(1,9)
+    while ((floor, room) in meeting_room_pk):
+        floor = random.randint(1,5)
+        room = random.randint(1,9)
+    meeting_room_pk.append((floor, room))
     meeting_room_data['floor'].append(floor)
     meeting_room_data['room'].append(room)
-    meeting_room_data['name'].append('#0' + str(floor) + '-0' + str(room))
+    # PDF says room name may be duplicated due to legacy reasons
+    meeting_room_data['name'].append(random.choice(['Database Meeting Room', 'BevSpot', 'Bynder', 'Cloud Technology Partners', 'Crayon', 'Continuum', 'InCrowd', 'MOO', 'Zaius']))
     room = room + 1
     meeting_room_data['department_id'].append(random.randint(0,19))
 # set_index removes the default incremental index
@@ -101,3 +105,20 @@ for _ in range(5):
 # Rename index to id for SQL table
 df_manager_data = pd.DataFrame(manager_data).set_index('id')
 df_manager_data.to_sql(name = 'managers', con = engine, if_exists = 'append')
+
+# Generate healthdeclarations table
+health_declaration_data = defaultdict(list)
+health_declaration_pk = []
+for _ in range(20):
+    id_value = random.randint(0, 19)
+    date = fake.date_between(start_date=datetime.date(2021,10,1), end_date='today')
+    while ((id_value, date) in health_declaration_pk):
+        id_value = random.randint(0, 19)
+        date = fake.date_between(start_date=datetime.date(2021,10,1), end_date='today')
+    health_declaration_pk.append((id_value, date))
+    health_declaration_data['id'].append(id_value)
+    health_declaration_data['date'].append(date)
+    health_declaration_data['temperature'].append(random.randint(340, 430) / 10)
+# set_index removes the default incremental index
+df_health_declaration_data = pd.DataFrame(health_declaration_data).set_index('id')
+df_health_declaration_data.to_sql(name = 'healthdeclarations', con = engine, if_exists = 'append')
