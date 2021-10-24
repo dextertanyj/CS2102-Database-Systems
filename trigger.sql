640af499199227303d61a5de52f51806df47f822
junlong4321 <desmondwongjunlong@yahoo.com.sg>
2021-10-24 15:48:39 +0800

    Update trigger 37

--- trigger.sql
+++ trigger.sql
@@ -1,80 +1,76 @@
  -- trigger 34 resigned employees no longer allowed to book or approve or attend any meeting, and cannot declare temperature 
 -- Meeting room booking or approval
-CREATE OR REPLACE FUNCTION check_resignation_status_for_booking_creation_or_approval() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION check_resignation_booking_create_approve() RETURNS TRIGGER AS $$
 DECLARE
 	creator_resignation_date DATE := NULL;
 	approver_resignation_date DATE := NULL;
 BEGIN
 	SELECT resignation_date INTO creator_resignation_date FROM Employees e WHERE e.id = NEW.creator_id;
 	SELECT resignation_date INTO approver_resignation_date FROM Employees e WHERE e.id = NEW.approver_id;
-	IF (creator_resignation_date IS NOT NULL AND approver_resignation_date IS NOT NULL) THEN
-		RAISE NOTICE 'Both employees booking and approving the meeting have resigned.';
-		RETURN NULL;
-	ELSIF (creator_resignation_date IS NOT NULL) THEN
-		RAISE NOTICE 'Employee attempting to book meeting has resigned.';
-		RETURN NULL;
+	IF (creator_resignation_date IS NOT NULL) THEN
+		RAISE EXCEPTION 'Employee attempting to book meeting has resigned.';
 	ELSIF (approver_resignation_date IS NOT NULL) THEN
-		RAISE NOTICE 'Employee attempting to approve meeting has resigned.';
-		RETURN NULL;
+		RAISE EXCEPTION 'Employee attempting to approve meeting has resigned.';
 	END IF ;
 	RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER can_create_or_approve_meeting
+DROP TRIGGER IF EXISTS check_resignation_booking_create_approve_trigger ON Bookings;
+CREATE TRIGGER check_resignation_booking_create_approve_trigger
 BEFORE INSERT OR UPDATE ON Bookings
-FOR EACH ROW EXECUTE FUNCTION check_resignation_status_for_booking_creation_or_approval();
+FOR EACH ROW EXECUTE FUNCTION check_resignation_booking_create_approve();
 
 -- Health declaration
-CREATE OR REPLACE FUNCTION check_resignation_status_for_health_declaration() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION check_resignation_health_declaration() RETURNS TRIGGER AS $$
 DECLARE
 	employee_resignation_date DATE := NULL;
 BEGIN
 	SELECT resignation_date INTO employee_resignation_date FROM Employees e WHERE e.id = NEW.id;
 	IF (employee_resignation_date IS NOT NULL) THEN
-		RAISE NOTICE 'Employee attempting to declare temperature has resigned.';
-		RETURN NULL;
+		RAISE EXCEPTION 'Employee attempting to declare temperature has resigned.';
 	END IF;
 	RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER can_declare_health
+DROP TRIGGER IF EXISTS check_resignation_health_declaration_trigger ON HealthDeclarations;
+CREATE TRIGGER check_resignation_health_declaration_trigger
 BEFORE INSERT OR UPDATE ON HealthDeclarations
-FOR EACH ROW EXECUTE FUNCTION check_resignation_status_for_health_declaration();
+FOR EACH ROW EXECUTE FUNCTION check_resignation_health_declaration();
 
 -- Attends meeting
-CREATE OR REPLACE FUNCTION check_resignation_status_for_attendance() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION check_resignation_attend() RETURNS TRIGGER AS $$
 DECLARE
 	employee_resignation_date DATE := NULL;
 BEGIN
 	SELECT resignation_date INTO employee_resignation_date FROM Employees e WHERE e.id = NEW.employee_id;
 	IF (employee_resignation_date IS NOT NULL) THEN
-		RAISE NOTICE 'Employee attempting to attend meeting has resigned.';
-		RETURN NULL;
+		RAISE EXCEPTION 'Employee attempting to attend meeting has resigned.';
 	END IF;
 	RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER can_attend_meeting
+DROP TRIGGER IF EXISTS check_resignation_attend_trigger ON Attends;
+CREATE TRIGGER check_resignation_attend_trigger
 BEFORE INSERT OR UPDATE ON Attends
-FOR EACH ROW EXECUTE FUNCTION check_resignation_status_for_attendance();
+FOR EACH ROW EXECUTE FUNCTION check_resignation_attend();
 
 -- Update meeting room capacity
-CREATE OR REPLACE FUNCTION check_resignation_status_for_updates() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION check_resignation_updates() RETURNS TRIGGER AS $$
 DECLARE
 	employee_resignation_date DATE := NULL;
 BEGIN
 	SELECT resignation_date INTO employee_resignation_date FROM Employees e WHERE e.id = NEW.manager_id;
 	IF (employee_resignation_date IS NOT NULL) THEN
-		RAISE NOTICE 'Employee attempting to update meeting room capacity has resigned.';
-		RETURN NULL;
+		RAISE EXCEPTION 'Employee attempting to update meeting room capacity has resigned.';
 	END IF;
 	RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER can_update_room
+DROP TRIGGER IF EXISTS check_resignation_updates_trigger ON Updates;
+CREATE TRIGGER check_resignation_updates_trigger
 BEFORE INSERT OR UPDATE ON Updates
-FOR EACH ROW EXECUTE FUNCTION check_resignation_status_for_updates();
+FOR EACH ROW EXECUTE FUNCTION check_resignation_updates();
